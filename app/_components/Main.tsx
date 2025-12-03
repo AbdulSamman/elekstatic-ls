21ee632f2315acb3fc9fdb01a7c4c3c0ce28ae91
AbdulSamman <sammanab@outlook.de>
2025-12-04 00:32:09 +0100

    swiper finished

--- app/_components/Main.tsx
+++ app/_components/Main.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { Pagination } from "swiper/modules";
 import "swiper/css/pagination";
+import Image from "next/image";
 
 function Main() {
   return (
@@ -13,18 +14,77 @@ function Main() {
       pagination={{ clickable: true }}
       modules={[Pagination]}>
       <SwiperSlide>
-        <div className="h-150 bg-red-500 flex items-center justify-center text-white text-2xl">
-          Slide 1
+        <div className="h-[600px] bg-blue-500 flex items-center justify-end text-white text-2xl">
+          <video
+            src="/media.mp4"
+            autoPlay
+            loop
+            playsInline
+            className="w-full"
+          />
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="h-150 bg-blue-500 flex items-center justify-center text-white text-2xl">
-          Slide 2
+        <div className="h-[600px] bg-red-500 flex items-center justify-center text-white text-2xl w-full">
+          <p className="text-2xl p-2 text-center w-3xl ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima eum
+            obcaecati consectetur eveniet veritatis recusandae iste unde tempora
+            libero quod sed architecto, officia corrupti facere natus!
+            Laudantium totam tenetur atque?
+          </p>
+          <div className="pt-8">
+            <Image
+              src="/produc-1.png"
+              loading="eager"
+              alt="produc-1"
+              width={320}
+              height={100}
+              className="object-contain"
+              priority
+            />
+          </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
-        <div className="h-150 bg-green-500 flex items-center justify-center text-white text-2xl">
-          Slide 3
+        <div className="h-[600px] bg-blue-500 flex items-center justify-end text-white text-2xl">
+          <p className="text-2xl p-2 text-center w-3xl ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima eum
+            obcaecati consectetur eveniet veritatis recusandae iste unde tempora
+            libero quod sed architecto, officia corrupti facere natus!
+            Laudantium totam tenetur atque?
+          </p>
+          <div className="pt-8">
+            <Image
+              src="/produc-2.png"
+              loading="eager"
+              alt="produc-2"
+              width={250}
+              height={100}
+              className="object-contain"
+              priority
+            />
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="h-[600px] bg-green-500 flex items-center justify-end text-white text-2xl">
+          <p className="text-2xl p-2 text-center w-3xl ">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima eum
+            obcaecati consectetur eveniet veritatis recusandae iste unde tempora
+            libero quod sed architecto, officia corrupti facere natus!
+            Laudantium totam tenetur atque?
+          </p>
+          <div className="pt-8">
+            <Image
+              src="/produc-3.png"
+              loading="eager"
+              alt="produc-1"
+              width={250}
+              height={100}
+              className="object-contain"
+              priority
+            />
+          </div>
         </div>
       </SwiperSlide>
     </Swiper>
